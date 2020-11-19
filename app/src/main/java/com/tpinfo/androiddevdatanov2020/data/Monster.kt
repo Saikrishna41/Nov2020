49ba15220c332d75f09f9b39047180e08c751a43
Saikrishna Ramalingam <saai.krrishna@gmail.com>
2020-11-19 17:04:12 -0600

    repo and data class setup and dependencies

---
+++ app/src/main/java/com/tpinfo/androiddevdatanov2020/data/Monster.kt
@@ -0,0 +1,11 @@
+package com.tpinfo.androiddevdatanov2020.data
+
+data class Monster(
+
+    val monsterName : String,
+    val imgfile : String,
+    val caption : String,
+    val description : String,
+    val scariness : Int,
+    val price : Double
+)
---
+++ app/src/main/java/com/tpinfo/androiddevdatanov2020/data/Repo/MonsterRepo.kt
@@ -0,0 +1,12 @@
+package com.tpinfo.androiddevdatanov2020.data.Repo
+
+import android.app.Application
+
+class MonsterRepo (val app : Application) {
+
+    init {
+
+    }
+
+
+}
