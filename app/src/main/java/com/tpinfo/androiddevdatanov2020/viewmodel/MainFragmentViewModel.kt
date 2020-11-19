65cda31488e27b6023ef0563d48d9b9b8274d6d3
Saikrishna Ramalingam <saai.krrishna@gmail.com>
2020-11-19 16:22:08 -0600

    frgment and view model added

---
+++ app/src/main/java/com/tpinfo/androiddevdatanov2020/viewmodel/MainFragmentViewModel.kt
@@ -0,0 +1,9 @@
+package com.tpinfo.androiddevdatanov2020.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainFragmentViewModel (app : Application) : AndroidViewModel(app) {
+
+
+}
